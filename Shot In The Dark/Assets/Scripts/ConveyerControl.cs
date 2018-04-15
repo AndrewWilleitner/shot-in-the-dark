4772c233c8390ce363ff740acc6b84df2701ca0e
AndrewWilleitner <andrewwilleitner@gmail.com>
2018-04-14 23:33:02 -0600

    update to moving belt
    The convayor belt now has a moving material and it can be powered on by a switch.

--- Shot In The Dark/Assets/Scripts/ConveyerControl.cs
+++ Shot In The Dark/Assets/Scripts/ConveyerControl.cs
@@ -3,22 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ConveyerControl : MonoBehaviour {
-	public float speed = 200f;
+    public float speed;
+    public Power[] powerSwitches;
+    private bool beltPowered;
 
+    private MeshRenderer rend;
+    private Vector2 matOffset;
+    private List<Rigidbody> rigidbodiesTouching = new List<Rigidbody>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        rend = transform.GetComponent<MeshRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        beltPowered = true;
+        for (int i = 0; i < powerSwitches.Length; i++)
+        {
+            if (!powerSwitches[i].powered)
+            {
+                beltPowered = false;
+            }
+        }
 
-	void OnCollisionStay(Collision obj){
-		float conveyorVelocity = speed * Time.deltaTime;
-		Rigidbody other = obj.gameObject.GetComponent<Rigidbody>();
-		other.velocity = conveyorVelocity * transform.forward;
-	}
+        if (beltPowered)
+        {
+            matOffset += new Vector2(0, speed / 120 * Time.deltaTime);
+            rend.materials[0].SetTextureOffset("_MainTex", matOffset);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (beltPowered)
+        {
+            float conveyorVelocity = speed * Time.deltaTime;
+            foreach (Rigidbody rig in rigidbodiesTouching) {
+                rig.velocity = conveyorVelocity * transform.forward;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        rigidbodiesTouching.Add(col.gameObject.GetComponent<Rigidbody>());
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        rigidbodiesTouching.Remove(col.gameObject.GetComponent<Rigidbody>());
+    }
 }
