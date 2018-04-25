96862e7852633e1a71655b00a37ad8d933f5ba06
Breann Evans <breannkay44@gmail.com>
2018-04-25 17:08:23 -0600

    Merge branch 'master' of https://github.com/AndrewWilleitner/shot-in-the-dark
    more sound

--- Shot In The Dark/Assets/Scripts/GlowStoneController.cs
+++ Shot In The Dark/Assets/Scripts/GlowStoneController.cs
@@ -9,6 +9,14 @@ public class GlowStoneController : MonoBehaviour {
 
     Light glowStoneLight;
 
+	private AudioSource source;
+	public AudioClip orbLandingSound;
+	public AudioClip orbPickupSound1;
+	public AudioClip orbPickupSound2;
+	public AudioClip orbPickupSound3;
+	public AudioClip orbPickupSound4;
+	private List<AudioClip> pickupSounds = new List<AudioClip> ();
+
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
@@ -18,6 +26,12 @@ public class GlowStoneController : MonoBehaviour {
 
         glowStoneLight.intensity = 0.5f;
         glowStoneLight.range = 8f;
+
+		source = GetComponent<AudioSource> ();
+		pickupSounds.Add (orbPickupSound1);
+		pickupSounds.Add (orbPickupSound2);
+		pickupSounds.Add (orbPickupSound3);
+		pickupSounds.Add (orbPickupSound4);
 	}
 	
 	// Update is called once per frame
@@ -28,12 +42,21 @@ public class GlowStoneController : MonoBehaviour {
 
     void OnCollisionStay(Collision collision)
     {
-        if (collision.transform.name != "Player")
-        {
-            glowStoneLight.intensity = 1f;
-            glowStoneLight.range = 15f;
-        }
+		if (collision.transform.name != "Player") {
+			glowStoneLight.intensity = 1f;
+			glowStoneLight.range = 15f;
+		} 
     }
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		if (collision.transform.name != "Player") {
+			source.PlayOneShot (orbLandingSound, .8f);
+		} 
+		if(collision.transform.name == "Player") {
+			source.PlayOneShot (pickupSounds [Random.Range (0, 3)], 1f);
+		}
+	}
 
 
     private void OutOfBoundsCheck()
