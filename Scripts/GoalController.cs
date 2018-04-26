a26771324f80a733eb753df419449023a483cbe0
TheSilo15 <35858577+TheSilo15@users.noreply.github.com>
2018-04-25 18:18:40 -0600

    FIxing The Issues
    For the love god, dont break

---
+++ Scripts/GoalController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalController : MonoBehaviour {
+
+    private Transform playerTransform;
+    private Transform cameraTransform;
+    private GameObject directionalLightObject;
+    private Light mainLight;
+
+    public Transform cameraZoomOutPoint;
+    public string nextSceneName;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerTransform = GameObject.Find("Player").transform;
+        cameraTransform = playerTransform.Find("Main Camera");
+
+        directionalLightObject = GameObject.Find("Directional Light");
+        mainLight = directionalLightObject.GetComponent<Light>();
+
+        RenderSettings.ambientIntensity = 0;
+        RenderSettings.reflectionIntensity = 0;
+        mainLight.intensity = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+
+    private IEnumerator ZoomOutCamera()
+    {
+        
+
+        Vector3 startPos = cameraTransform.position;
+        Quaternion startRot = cameraTransform.rotation;
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / 5f;
+            cameraTransform.position = Vector3.Lerp(startPos, cameraZoomOutPoint.position, t);
+            cameraTransform.rotation = Quaternion.Lerp(startRot, cameraZoomOutPoint.rotation, t);
+            RenderSettings.ambientIntensity = t;
+            RenderSettings.reflectionIntensity = t;
+            mainLight.intensity = t * 0.5f;
+
+            yield return new WaitForEndOfFrame();
+        }
+        cameraTransform.position = cameraZoomOutPoint.position;
+        cameraTransform.rotation = cameraZoomOutPoint.rotation;
+        RenderSettings.ambientIntensity = 1;
+        RenderSettings.reflectionIntensity = 1;
+        mainLight.intensity = 0.5f;
+
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.transform.tag == "Player")
+        {
+            cameraTransform.parent = null;
+            Destroy(playerTransform.gameObject);
+            StartCoroutine("ZoomOutCamera");
+        }
+    }
+}
---
+++ Scripts/SwitchTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTrigger : MonoBehaviour {
+
+    private Power power;
+    private LineRenderer lineRender;
+    private AudioSource source;
+
+    public Material powerOnMaterial;
+    public Material powerOffMaterial;
+    public AudioClip switchOnSound;
+    public AudioClip switchOffSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        power = transform.GetComponent<Power>();
+        lineRender = transform.GetComponent<LineRenderer>();
+        source = GetComponent<AudioSource>();
+
+        if (power.powered)
+            lineRender.material = powerOnMaterial;
+        else
+            lineRender.material = powerOffMaterial;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.transform.tag == "GlowStone")
+        {
+            source.PlayOneShot(switchOnSound);
+            power.powered = true;
+            lineRender.material = powerOnMaterial;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.transform.tag == "GlowStone")
+        {
+            source.PlayOneShot(switchOffSound);
+            power.powered = false;
+            lineRender.material = powerOffMaterial;
+        }
+    }
+}
